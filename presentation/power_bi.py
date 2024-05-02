b4f2e222feb2e821e9abb97fd477ecd8d399e9b0
Vermes Balázs <61848817+bvermes@users.noreply.github.com>
2024-05-03 01:41:29 +0200

    power bi start works

--- presentation/power_bi.py
+++ presentation/power_bi.py
@@ -6,7 +6,7 @@ def open_power_bi_dashboard():
     POWER_BI_DESKTOP_PATH = r"C:\Users\Balazs\AppData\Local\Microsoft\WindowsApps\Microsoft.MicrosoftPowerBIDesktop_8wekyb3d8bbwe\PBIDesktopStore.exe"
     try:
         dashboard_path = os.path.join(
-            os.path.abspath(os.path.dirname(__file__)), "presentation", "dashboard.pbix"
+            os.path.abspath(os.path.dirname(__file__)), "dashboard.pbix"
         )
 
         subprocess.Popen([POWER_BI_DESKTOP_PATH, dashboard_path])
